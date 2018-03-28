9876e4ef6a1f7f3db6fada67f073fda78522b8ab
Untitled <john.d.reddock@gmail.com>
2018-03-28 18:13:41 -0400

    disabled movement without key press

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ class Hero(Character):
         self.image = pygame.image.load('images/hero.png')
         self.x = 240
         self.y = 224
-        self.speed = 2
+        self.base_speed = 2
 
     def update(self):
         self.x += self.speed_x
@@ -25,45 +25,62 @@ class Monster(Character):
     def __init__(self):
         super(Monster, self).__init__()
         self.image = pygame.image.load('images/monster.png')
-        self.x = 240
-        self.y = 224
-        self.speed = 2
+        self.x = 53
+        self.y = 50
+        self.base_speed = 2
         
 
 def main():
-    width = 800
-    height = 600
+    width = 512
+    height = 480
     blue_color = (97, 159, 182)
-
     pygame.init()
     screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('My Game')
+    background_image = pygame.image.load('images/background.png').convert_alpha()
+    pygame.display.set_caption('Monster Catch!')
     clock = pygame.time.Clock()
-    hero_img = pygame.image.load('images/hero.png')
-    
-    monster_img = pygame.image.load('images/monster.png')
-    background_image = pygame.image.load('images/background.png')
-    background_image = pygame.transform.scale(background_image,(800,600))
-    music = pygame.mixer.music.load("sounds/music.wav")
-    pygame.mixer.music.play(1, 0.0)
-    hero = Hero()
-    
-    monster = Monster()
-    
+          
 
     # Game initialization
-
+    hero = Hero()    
+    monster = Monster()
     stop_game = False
+    timer_count = 119
+
     while not stop_game:
         for event in pygame.event.get():
-
+            
             # Event handling
 
             if event.type == pygame.QUIT:
                 stop_game = True
 
+            # If no keys are pressed no movement by hero
+            if event.type == pygame.KEYUP:
 
-        # Game logic
+                if event.key == pygame.K_DOWN: #274
+                    hero.speed_y = 0                
+                elif event.key == pygame.K_UP: #273
+                    hero.speed_y = 0                
+                elif event.key == pygame.K_LEFT: #276
+                    hero.speed_x = 0                
+                elif event.key == pygame.K_RIGHT: #275
+                    hero.speed_x = 0
+
+            #Key held down moves hero
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_DOWN:
+                    hero.speed_y = hero.base_speed
+                elif event.key == pygame.K_UP:
+                    hero.speed_y = -hero.base_speed
+                elif event.key == pygame.K_LEFT:
+                    hero.speed_x = -hero.base_speed
+                elif event.key == pygame.K_RIGHT:
+                    hero.speed_x = hero.base_speed
+                
+        # Update position of hero every loop
+        hero.update()
 
         # Draw background
         screen.blit(background_image, [0,0])
@@ -73,7 +90,7 @@ def main():
         hero.draw(screen)        
         monster.draw(screen)       
         pygame.display.update()
-        
+
         clock.tick(60)
 
     pygame.quit()
